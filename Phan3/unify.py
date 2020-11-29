32ea200aa6e3130fac5de11c97483ecc8e9cce3c
Trần Tuấn Đạt <62866371+bobyba2000@users.noreply.github.com>
2020-11-29 13:42:09 +0700

    Update unify.py

--- Phan3/unify.py
+++ Phan3/unify.py
@@ -13,7 +13,7 @@ def unify(x, y, theta):
   elif (Fact.is_fact_in_fact(x) and Fact.is_fact_in_fact(y)):
     return unify(x.get_args(), y.get_args(), unify(x.get_op(), y.get_op(), theta))
   elif (Fact.is_list_of_fact(x) and Fact.is_list_of_fact(y)):
-    return unify(x.get_args()[1:], y.get_args()[1:], unify(x.get_args()[0], y.get_args()[0], theta))
+    return unify(x[1:], y[1:], unify(x[0], y[0], theta))
   return False
 
 def unifyVar(var, x, theta):
