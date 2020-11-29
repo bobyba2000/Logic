8a0566aa39a1e3778389e012bda7aae234f54bbc
Trần Tuấn Đạt <62866371+bobyba2000@users.noreply.github.com>
2020-11-29 12:52:35 +0700

    Create substitution.py

---
+++ Phan3/substitution.py
@@ -0,0 +1,24 @@
+class Substitution:
+  def __init__(self):
+    self.vars = []
+    self.vals = []
+
+  def containVar(self, var):
+    return var in self.vars
+
+  def containVal(self, val):
+    return val in self.vals
+
+  def getValBaseOnVar(self, var):
+    for i in range(len(self.vars)):
+      if (self.vars[i] == var):
+        return self.vals[i]
+
+  def getVarBaseOnVal(self, val):
+    for i in range(len(self.vals)):
+      if (self.vals[i] == val):
+        return self.vars[i]
+
+  def add(self, var, val):
+    self.vars.append(var)
+    self.vals.append(val)
